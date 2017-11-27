89a6f185b4fb42cae5c52e9e5e5499d91a07a338
workstation-1 <workstation-1@automate-demo.com>
2017-11-27 02:59:15 +0000

    Release version 0.2.0

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+    content "<h1>Hello, world!</h1>
+    
+    <h2>IPADDRESS: #{node['ipaddress']} </h2>
+    "
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -13,6 +13,10 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe command('curl localhost') do
+  its('stdout') { should match ('Hello, world')}
 end
